2c878bae17c8fc8129f5bbb8a7a858d2db10bf2c
Alexander <cph-an178@cphbusiness.dk>
2017-10-02 13:38:25 +0200

    Added server

--- CourseAssignment2/src/main/java/tcp/Server.java
+++ CourseAssignment2/src/main/java/tcp/Server.java
@@ -1,5 +1,32 @@
 package tcp;
 
+import java.net.*;
+import java.io.*;
+
 public class Server {
-    // Yo wtf
+    
+    public Server(String ip, int port) {
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(ip, port));
+
+            System.out.println("Server log: Server created...");
+            System.out.println("Server log: Listening for clients on port " + port + "...");
+
+            try {
+                System.out.println("Server log: Waiting for connections...");
+
+                while (true) {
+                    new ServerClientThread(serverSocket.accept());
+                }
+            } catch (Exception e) {
+                System.err.println("Server log: Accepting connection failed...");
+            }
+        } catch (Exception e) {
+            System.out.println("Server log: Could not listen on port: " + port + "...");
+        }
+    }
+ 
 }
